736715df53d20381fbf6ae0d1158af283ac0843b
andrew <w0402993@nscc.ca>
2025-10-21 20:43:10 -0300

    added app.UseStaticFiles(); to program.cs

--- TicketHive/Program.cs
+++ TicketHive/Program.cs
@@ -41,6 +41,10 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Enable static files middleware to serve static files
+app.UseStaticFiles();
+
 app.UseRouting();
 
 // Add a user authentication
